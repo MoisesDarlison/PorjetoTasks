b2a466eac585b1969f250b79509eecd9983558ff
Darlison <Darlison001@gmail.com>
2020-12-07 20:48:57 -0300

    recriando base de dados e reorganizando arq

--- config/connections.js
+++ config/connections.js
@@ -1,17 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = {
-    dialect: "postgres",
-    host: 'localhost',
-    username: process.env.USER_DB,
-    password: process.env.PASSWORD_DB,
-    database: process.env.NAME_DB,
-    port: process.env.PORT_DB,
-    define: {
-        timestamps: true,
-        underscore: true,
-        freezeTableName:true,
-    },
-}
+const dbConfig = require('./config')
 
 const connection = new Sequelize(dbConfig);
 
